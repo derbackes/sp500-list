cf8411e8ddd2b118d1a7ecb8e8ee825097c9bb01
Michael Backes <npm@michaelbackes.com>
2015-09-20 18:06:40 +0200

    passing through the error object from request if it fails

--- index.js
+++ index.js
@@ -13,31 +13,37 @@ module.exports = {
 
     // Go grab the webpage
     var returnList = request(url, function(err, resp, body){
-      // The array of stocks
-      var stockList = [];
+      if (!err && resp.statusCode == 200) {
 
-      // Get cheerio started
-      $ = cheerio.load(body);
-      // Sep, 2015 - There are two tables on the page with data
-      $('wikitable sortable, tr td:first-child').each(function(i, element) {
-        // If I see the start of the second table, stop adding to the list
-        if($(this).text().indexOf("Date") > -1 ) {
-          isPastList = true;
-        }
-        if(!isPastList) {
-          // Then we are in the list of S&P stocks, keep going
-          //console.log($(this).text());
-          // Double check that it is not empty
-          if($(this).text().trim()) {
-            var data = {
-              ticker: $(this).text()
-            };
-            stockList.push(data);
+        // The array of stocks
+        var stockList = [];
+
+        // Get cheerio started
+        $ = cheerio.load(body);
+        // Sep, 2015 - There are two tables on the page with data
+        $('wikitable sortable, tr td:first-child').each(function(i, element) {
+          // If I see the start of the second table, stop adding to the list
+          if($(this).text().indexOf("Date") > -1 ) {
+            isPastList = true;
           }
-        }
-      });
-      //console.log(stockList);
-      callback(stockList);
+          if(!isPastList) {
+            // Then we are in the list of S&P stocks, keep going
+            //console.log($(this).text());
+            // Double check that it is not empty
+            if($(this).text().trim()) {
+              var data = {
+                ticker: $(this).text()
+              };
+              stockList.push(data);
+            }
+          }
+        });
+        //console.log(stockList);
+        callback(stockList);
+      } else {
+        callback(null, err);
+      }
+
     });
   },
 
